e55ebc17531fea69f2d3969d33914fe3ad7125ea
Tony Butler <spudz76@gmail.com>
2021-05-25 06:10:26 -0600

    Add every Apple AMD GPU type

--- src/backend/opencl/generators/ocl_generic_rx_generator.cpp
+++ src/backend/opencl/generators/ocl_generic_rx_generator.cpp
@@ -63,6 +63,7 @@ bool ocl_generic_rx_generator(const OclDevice &device, const Algorithm &algorith
     case OclDevice::Navi_10:
     case OclDevice::Navi_12:
     case OclDevice::Navi_14:
+    case OclDevice::Navi_21:
         gcnAsm = true;
         isNavi = true;
         break;
